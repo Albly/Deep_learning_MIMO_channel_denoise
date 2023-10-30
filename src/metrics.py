faab826b6e5b217d082335f645ee2e3bd45c7e26
Alexander Blagodarnyi <albly@MacBook-Pro-Alexander.local>
2023-10-30 11:53:10 +0300

    bug has been fixed

--- src/metrics.py
+++ src/metrics.py
@@ -15,6 +15,6 @@ def complex_LOG_MSE(x_real, x_hat):
 
     assert x_real.shape == x_hat.shape, 'Sizes of both values must be the same, but got {0} and {1} instead'.format(x_real.shape, x_hat.shape)
     
-    eps = torch.tensor([0.0000001])
+    eps = torch.tensor([1.00], device= x_real.device)
     mlse = torch.mean(10*torch.log10(torch.abs(x_real-x_hat)**2 + eps))
     return mlse
